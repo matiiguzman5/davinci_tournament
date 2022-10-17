7530cf5d9626071db5ae7498a70c14cb81ffa3ce
escuela@davinci.edu.ar <116008988+matiiguzman5@users.noreply.github.com>
2022-10-17 12:50:02 -0300

    chore:add ultimo

--- src/Clase17_10.java
+++ src/Clase17_10.java
@@ -3,19 +3,12 @@ import java.util.Date;
 public class Clase17_10 {
     public static void main(String[] args) {
 
-        Season torneoDaVinci = new Season("Torneo 2022, 2do cuatri");
-        torneoDaVinci.setName("Torneo 2022, 2do cuatri");
-        Date comienzoDeCursada = new Date(2022/7/18);
-        comienzoDeCursada.setYear(2022);
-        comienzoDeCursada.setMonth(7); //Enero 0, Agosto 7
-        comienzoDeCursada.setDate(18);
-        torneoDaVinci.setStartDate(comienzoDeCursada);
+        Date comienzoDeCursada = new Date(2022,7,18);
+        
 
-        Date finDeCursada = new Date(2022/11/5);
-        finDeCursada.setYear(2022);
-        finDeCursada.setMonth(11); //Enero 0, Dic 11
-        finDeCursada.setDate(5);
-        torneoDaVinci.setEndDate(finDeCursada);
+        Date finDeCursada = new Date(2022,11,5);
+
+        Season torneoDaVinci = new Season("Torneo 2022, 2do cuatri" , comienzoDeCursada, finDeCursada);
+
     }
-    Season torneoDaVinci = new Season("Torneo 2022, 2do cuatri" , 2022/7/18);
 }
